0301ef8e5e7af8d2da46eb0fa004b5dfc5325373
hellenribas <tchwtrabalho@gmail.com>
2022-08-22 15:14:02 -0300

    feat: testing /matches tdd

---
+++ app/backend/src/tests/matches.test.ts
@@ -0,0 +1,188 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Match from '../database/models/matches';
+
+import { Response } from 'superagent';
+import ITeam from '../interface/ITeam';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const matchMock: IMatch[] =  [
+  {
+    "id": 1,
+    "homeTeam": 1,
+    "homeTeamGoals": 1,
+    "awayTeam": 8,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+    "teamHome": {
+      "teamName": "Team2"
+    },
+    "teamAway": {
+      "teamName": "Team1"
+    }
+  },
+  {
+    "id": 2,
+    "homeTeam": 2,
+    "homeTeamGoals": 2,
+    "awayTeam": 9,
+    "awayTeamGoals": 0,
+    "inProgress": true,
+    "teamHome": {
+      "teamName": "Team3"
+    },
+    "teamAway": {
+      "teamName": "Team4"
+    }
+  }
+];
+
+const matchMockInProgress: IMatch[] =  [
+  {
+    "id": 1,
+    "homeTeam": 1,
+    "homeTeamGoals": 1,
+    "awayTeam": 8,
+    "awayTeamGoals": 1,
+    "inProgress": true,
+    "teamHome": {
+      "teamName": "Team2"
+    },
+    "teamAway": {
+      "teamName": "Team1"
+    }
+  },
+  {
+    "id": 2,
+    "homeTeam": 2,
+    "homeTeamGoals": 2,
+    "awayTeam": 9,
+    "awayTeamGoals": 0,
+    "inProgress": true,
+    "teamHome": {
+      "teamName": "Team3"
+    },
+    "teamAway": {
+      "teamName": "Team4"
+    }
+  }
+];
+
+const matchMockNoInProgress: IMatch[] =  [
+  {
+    "id": 1,
+    "homeTeam": 1,
+    "homeTeamGoals": 1,
+    "awayTeam": 8,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+    "teamHome": {
+      "teamName": "Team2"
+    },
+    "teamAway": {
+      "teamName": "Team1"
+    }
+  },
+  {
+    "id": 2,
+    "homeTeam": 2,
+    "homeTeamGoals": 2,
+    "awayTeam": 9,
+    "awayTeamGoals": 0,
+    "inProgress": false,
+    "teamHome": {
+      "teamName": "Team3"
+    },
+    "teamAway": {
+      "teamName": "Team4"
+    }
+  }
+];
+
+describe('/matches no filter', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(Match, "findAll")
+      .resolves(matchMock as Match[]);
+  });
+
+  after(()=>{
+    sinon.restore();
+  })
+
+  it('should return status 200', async () => {
+    chaiHttpResponse = await chai
+       .request(app).get('/matches').send();
+
+    expect(chaiHttpResponse.status).to.equal(200);
+  });
+
+  it('should return matches', async () => {
+    chaiHttpResponse = await chai
+    .request(app).post('/matches');
+    expect(chaiHttpResponse.body).to.equal(matchMock);
+  });
+});
+
+describe('/matches in progress', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(Match, "findAll")
+      .resolves(matchMockInProgress as Match[]);
+  });
+
+  after(()=>{
+    sinon.restore();
+  })
+
+  it('should return status 200', async () => {
+    chaiHttpResponse = await chai
+       .request(app).get('/matches').send();
+
+    expect(chaiHttpResponse.status).to.equal(200);
+  });
+
+  it('should return matches', async () => {
+    chaiHttpResponse = await chai
+    .request(app).post('/matches');
+    expect(chaiHttpResponse.body).to.equal(matchMockInProgress);
+  });
+});
+
+describe('/matches no in progress', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(Match, "findAll")
+      .resolves(matchMockNoInProgress as Match[]);
+  });
+
+  after(()=>{
+    sinon.restore();
+  })
+
+  it('should return status 200', async () => {
+    chaiHttpResponse = await chai
+       .request(app).get('/matches').send();
+
+    expect(chaiHttpResponse.status).to.equal(200);
+  });
+
+  it('should return matches', async () => {
+    chaiHttpResponse = await chai
+    .request(app).post('/matches');
+    expect(chaiHttpResponse.body).to.equal(matchMockNoInProgress);
+  });
+});
