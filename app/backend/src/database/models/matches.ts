4ecac44e90549952ad9822900daad32a02468663
hellenribas <tchwtrabalho@gmail.com>
2022-08-22 15:56:04 -0300

    feat: created model and migration matches

---
+++ app/backend/src/database/models/matches.ts
@@ -0,0 +1,54 @@
+import { INTEGER, Model } from 'sequelize';
+import db from '.';
+import Team from './teams';
+
+class Match extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: INTEGER,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'match',
+  timestamps: false,
+  underscored: true,
+});
+
+Match.belongsTo(Team, {
+  foreignKey: 'homeTeam',
+  as: 'teamHome',
+});
+
+Match.belongsTo(Team, {
+  foreignKey: 'awayTeam',
+  as: 'teamAway',
+});
+
+export default Match;
